f1787b03bf69ffc5b7110ae96ee41647d7bf7b56
LC1B_21_ホリケ_ハヤト <k024g0019@g.neec.ac.jp>
2025-04-14 20:03:26 +0900

    kamataengineを入れた

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,52 @@
+#include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
+
+// KamataEngineの名前空間を使用
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// ゲームシーンのインスタンスを生成
+	GameScene* gameScene = new GameScene();
+
+	// DirectXCommonのインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
+	// エンジンの初期化
+	Initialize(L"LE2A_13_ホリケ_ハヤト_AL3_kakuninn_01_01");
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (Update()) {
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	Finalize();
+
+	// ゲームシーンの解放
+	delete gameScene;
+	// nullpttrの代入
+	gameScene = nullptr;
+
+	return 0;
+}
